26968e4a380741beb56dda79587e7ff14d006c32
Navya Neethi S <112698395+navyaneethi@users.noreply.github.com>
2024-05-05 14:46:29 +0530

    Add files via upload

---
+++ static/js/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCO3yjzquAAGOp3O09V1pr51yqysNALeo0",
+        authDomain: "stegx-148c0.firebaseapp.com",
+        projectId: "stegx-148c0",
+        storageBucket: "stegx-148c0.appspot.com",
+        messagingSenderId: "436928947016",
+        appId: "1:436928947016:web:db0f12780156e74062897d",
+        measurementId: "G-HVER2D87HQ"
+  };
+  firebase.initializeApp(config);
